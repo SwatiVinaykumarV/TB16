faf3fe3c6dc28ffaa97eb36589da9baa7cdd511f
Swati Vinaykumar V <Swati Vinaykumar V@LAPTOP-FCRPM86O>
2022-08-01 18:53:11 +0530

    this check

--- GIT/src/Code/Demo.java
+++ GIT/src/Code/Demo.java
@@ -6,6 +6,15 @@ public class Demo {
 	{
 		System.out.println("Hi m1");
 	}
+	public void m2()
+	{
+		System.out.println("Hi m2");
+	}
+	public void m3()
+	{
+		System.out.println("Hi m3");
+	}
+	
 	
 	public static void main(String[] args) 
 	{
