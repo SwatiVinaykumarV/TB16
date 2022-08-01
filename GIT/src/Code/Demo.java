8926372761a27e3b7e4148440183cde9b2501433
SwatiVinaykumarV <110389890+SwatiVinaykumarV@users.noreply.github.com>
2022-08-01 18:37:54 +0530

    Create Demo.java

--- GIT/src/Code/Demo.java
+++ GIT/src/Code/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("This is my 1st Git");
+		System.out.println("This is my 1st Git..ok");
 		public void m1
 		{
 			
