965b825b5d4f118f15b2054fa891d27cd86cb427
Swati Vinaykumar V <Swati Vinaykumar V@LAPTOP-FCRPM86O>
2022-08-02 16:54:40 +0530

    this is new

---
+++ GIT/src/Code/Project.java
@@ -0,0 +1,15 @@
+package Code;
+
+public class Project 
+{
+	public void method1()
+	{
+		System.out.println("");
+	}
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("this is new Git project");
+	}
+
+}
